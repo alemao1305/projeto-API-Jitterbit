182e5a2b74c6f0b73ab57161e406c44cf14ef27f
Olinto de Mello <73202297+alemao1305@users.noreply.github.com>
2026-03-08 17:05:14 -0300

    repado + docker

--- config/db.js
+++ config/db.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/orderdb', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        await mongoose.connect('mongodb://admin:admin123@localhost:27017', {
+            
         });
         console.log('MongoDB conectado!');
     } catch (error) {
